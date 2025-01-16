b7f938f20095d3cc60598cf854fd06e6a2690bfc
Simonmie <16605913446@163.com>
2025-01-16 17:41:46 +0800

    feat: docs初始化

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,5 +1,6 @@
 module.exports = {
   root: true,
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 2020,
     sourceType: 'module',
@@ -16,12 +17,23 @@ module.exports = {
     'eslint:recommended',
     'plugin:vue/vue3-recommended',
     'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ['vue', 'prettier'],
+  plugins: ['vue', 'prettier', '@typescript-eslint'],
   rules: {
+    // 确保所有规则都正确配置
     'vue/no-unused-vars': 'warn',
     'no-console': 'warn',
     'prettier/prettier': ['error', { singleQuote: true }],
+
+    // TS 相关配置
+    '@typescript-eslint/no-unused-vars': 'warn', // 禁止定义未使用的变量
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-ts-expect-error': 'error', // 禁止使用 @ts-ignore
+    '@typescript-eslint/no-explicit-any': 'off', // 禁止使用 any 类型
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-namespace': 'off', // 禁止使用自定义 TypeScript 模块和命名空间。
+    '@typescript-eslint/semi': 'off',
   },
   overrides: [
     {
