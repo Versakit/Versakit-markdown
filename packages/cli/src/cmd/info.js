677241f50e07cf27ea6d6e893b85d77882923485
Jannik <1337741710@qq.com>
2025-01-19 13:50:26 +0800

    feat: CLIWIP

---
+++ packages/cli/src/cmd/info.js
@@ -0,0 +1,55 @@
+import colors from 'colors'
+import os from 'os'
+import { execSync } from 'child_process'
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const figlet = require('figlet')
+
+function getPnpmVersion() {
+  const result = execSync('pnpm -v').toString().trim()
+  return result
+}
+
+function getNestCLIVersion() {
+  const result = execSync('v --version').toString().trim()
+
+  return result
+}
+
+export const info = () => {
+  console.log(
+    figlet.textSync(
+      'Versakit',
+      {
+        font: 'Ghost',
+        horizontalLayout: 'default',
+        verticalLayout: 'default',
+        width: 120,
+        whitespaceBreak: true,
+      },
+      function (err, data) {
+        if (err) {
+          console.dir(err)
+          return
+        }
+        console.log(data)
+      },
+    ),
+  )
+
+  console.log(colors.green('System Information'))
+
+  const systemInfo = [
+    `OS Version     : ${os.type()} ${os.release()}`,
+    `NodeJS Version : ${process.version}`,
+    `PNPM Version   : ${getPnpmVersion()}`,
+  ]
+
+  systemInfo.forEach((info) => {
+    console.log(colors.cyan(info))
+  })
+
+  console.log(colors.green('Versakit MarkDown CLI'))
+  console.log(
+    colors.cyan(`Versakit MarkDown CLI Version : ${getNestCLIVersion()}`),
+  )
+}
