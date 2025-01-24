c2390bf64f1e1552cfebbc278b030af2f372e400
Jannik <1337741710@qq.com>
2025-01-24 12:50:00 +0800

    feat: 组件拆分

---
+++ packages/vue/src/components/editor/index.vue
@@ -0,0 +1,87 @@
+<template>
+  <div>
+    <div class="editor" contenteditable="true" ref="editorRef" />
+    <div class="status-bar">
+      <div>行: {{ currentRow }}, 列: {{ currentColumn }}</div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue'
+import store from '../../../store/store.ts'
+import type { RichProps } from '../../type.ts'
+
+const editorRef = ref<HTMLElement | null>(null)
+const currentRow = ref(1)
+const currentColumn = ref(1)
+
+withDefaults(defineProps<RichProps>(), {
+  value: '',
+})
+
+const emit = defineEmits(['update:value']) // 触发更新事件
+
+// 更新光标位置
+const updateCursorPosition = () => {
+  const selection = window.getSelection()
+  if (!selection || !selection.focusNode || !editorRef.value) return
+
+  let node = selection.focusNode
+  let offset = selection.focusOffset
+
+  currentColumn.value = offset + 1
+
+  let currentNode = editorRef.value.firstChild
+  let row = 1
+
+  while (currentNode && currentNode !== node) {
+    if (currentNode.nodeType === Node.ELEMENT_NODE) {
+      const element = currentNode as HTMLElement
+      if (
+        element.tagName.toLowerCase() === 'p' ||
+        element.tagName.toLowerCase() === 'div' ||
+        element.tagName.toLowerCase() === 'br'
+      ) {
+        row++
+      }
+    }
+    currentNode = currentNode.nextSibling
+  }
+
+  currentRow.value = row
+}
+
+// 定义更新函数，处理状态更新时的逻辑
+const customUpdateFunction = () => {
+  const textContent = editorRef.value?.textContent || ''
+
+  // 更新父组件的 value
+  emit('update:value', textContent)
+}
+
+// 注册观察者到 store
+const unsubscribe = store.attach(customUpdateFunction)
+
+// 生命周期钩子
+onMounted(() => {
+  document.addEventListener('selectionchange', updateCursorPosition)
+  editorRef.value?.addEventListener('input', customUpdateFunction)
+})
+
+onUnmounted(() => {
+  document.removeEventListener('selectionchange', updateCursorPosition)
+  editorRef.value?.removeEventListener('input', customUpdateFunction)
+  unsubscribe()
+})
+</script>
+
+<style scoped>
+.editor {
+  @apply outline-none p-4 min-h-[400px];
+}
+
+.status-bar {
+  @apply absolute bottom-0 left-0 w-full text-[12px] text-gray-500 p-2 dark:text-white dark:bg-zinc-800 dark:border-gray-600;
+}
+</style>
--- packages/vue/src/index.vue
+++ packages/vue/src/index.vue
@@ -2,110 +2,32 @@
   <div>
     <div class="rich-text-editor">
       <ToolBar />
-      <div class="editor" contenteditable="true" ref="editorRef" />
 
-      <div class="status-bar">
-        <div>行: {{ currentRow }}, 列: {{ currentColumn }}</div>
-      </div>
+      <Editor :value="value" @update:value="updateValue" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { onBeforeUnmount, onMounted, onUpdated, onUnmounted, ref } from 'vue'
 import ToolBar from './components/toolbar/index.vue'
-import store from '../store/store'
-import type { RichProps } from './type.ts'
+import Editor from './components/editor/index.vue'
+import type { RichProps } from './type'
 
 defineOptions({ name: 'VerRichEditor' })
-
-const editorRef = ref<HTMLElement | null>(null)
-const currentRow = ref(1)
-const currentColumn = ref(1)
 
 withDefaults(defineProps<RichProps>(), {
   value: '',
 })
 
-const emit = defineEmits(['update:value'])
+const emit = defineEmits(['update:value']) // 触发更新事件
 
-// 更新光标位置
-const updateCursorPosition = () => {
-  const selection = window.getSelection()
-  if (!selection || !selection.focusNode || !editorRef.value) return
-
-  let node = selection.focusNode
-  let offset = selection.focusOffset
-
-  // 更新列号
-  currentColumn.value = offset + 1
-
-  // 从编辑器的根节点开始遍历
-  let currentNode = editorRef.value.firstChild
-  let row = 1 // 初始行号为 1
-
-  // 遍历所有节点，直到到达光标所在的节点
-  while (currentNode && currentNode !== node) {
-    if (currentNode.nodeType === Node.ELEMENT_NODE) {
-      const element = currentNode as HTMLElement
-      if (
-        element.tagName.toLowerCase() === 'p' ||
-        element.tagName.toLowerCase() === 'div' ||
-        element.tagName.toLowerCase() === 'br'
-      ) {
-        row++
-      }
-    }
-    currentNode = currentNode.nextSibling
-  }
-
-  // 更新行号
-  currentRow.value = row
+const updateValue = (newValue: string) => {
+  emit('update:value', newValue) // 通知父组件更新值
 }
-
-// 定义更新函数，处理状态更新时的逻辑
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const customUpdateFunction = (_observable: any) => {
-  const textContent = editorRef.value?.textContent || ''
-
-  emit('update:value', textContent)
-}
-
-// 注册观察者到 store
-const unsubscribe = store.attach(customUpdateFunction)
-
-// 生命周期钩子
-onMounted(() => {
-  document.addEventListener('selectionchange', updateCursorPosition)
-  editorRef.value?.addEventListener('input', updateCursorPosition)
-})
-
-onUpdated(() => {
-  if (editorRef.value) {
-    store.actions({ editorRef: editorRef.value })
-  }
-})
-
-onBeforeUnmount(() => {
-  unsubscribe()
-})
-
-onUnmounted(() => {
-  document.removeEventListener('selectionchange', updateCursorPosition)
-  editorRef.value?.removeEventListener('input', updateCursorPosition)
-})
 </script>
 
 <style scoped>
 .rich-text-editor {
   @apply relative rounded-sm border-2 h-full w-full dark:bg-zinc-800 dark:text-white dark:border-gray-600;
-}
-
-.editor {
-  @apply outline-none p-4 min-h-[400px];
-}
-
-.status-bar {
-  @apply absolute bottom-0 left-0 w-full text-[12px] text-gray-500 p-2  dark:text-white  dark:bg-zinc-800 dark:border-gray-600;
 }
 </style>
